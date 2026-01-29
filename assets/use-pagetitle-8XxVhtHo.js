31cd59ffbc39a3f8b81e1861d90739172f694859
Li-Zou <Li-Zou@users.noreply.github.com>
2026-01-29 13:19:50 +0000

    [ci] deploy site e86117d24e206353e70aaa41c4d9f6fdafc20927

---
+++ assets/use-pagetitle-8XxVhtHo.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-CVfxetPS.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-BAeHCY3v.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-CsfcVbTB.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
